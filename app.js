6fa4157fec63bd13690cab7b3f132f9b219ccefd
phantware <phantmoney2011@gmail.com>
2020-01-11 23:39:30 +0100

    babel build code

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-// import express from "express";
+import express from "express";
 
-const express = require("express");
+// const express = require("express");
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World!!");
 });
 
-port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, console.log(`App is listening at ${port}`));
