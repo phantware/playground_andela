98ca3426ed39afb42c0b71554e6531db339bb722
phantware <phantmoney2011@gmail.com>
2020-03-19 07:56:14 +0100

    upload location changed from hardcoded absolute to dynamics

--- src/routes/article.js
+++ src/routes/article.js
@@ -4,9 +4,9 @@ import path from 'path';
 import articleController from '../controllers/article';
 import authToken from '../middleware/auth';
 import articleMiddleware from '../middleware/article';
-import pool from '../config/db';
+// import pool from '../config/db';
 
-const fs = require('fs');
+// const fs = require('fs');
 
 const location = path.join(__dirname, 'hhh/a/b');
 console.log('location', location);
@@ -41,28 +41,28 @@ router.post('/img', upload.single('avatar'), (req, res) => {
   res.status(200).send('correct guy');
 });
 
-router.post('/profile', upload.single('avatar'), (req, res) => {
+// router.post('/profile', upload.single('avatar'), (req, res) => {
 //    req.file is the `avatar` file
-  // req.body will hold the text fields, if there were any
-  // ({ upload } = req.file);
-  console.log('this is file', req.file);
-  console.log('this is req.body', req.body);
-  let result;
-  const newFilename = `${req.file.filename}-${req.file.originalname}`;
-  fs.rename(req.file.path, `${req.file.destination}${newFilename}`, async (err) => {
-    if (err) return res.send(`ERROR: ${err}`);
+// req.body will hold the text fields, if there were any
+// ({ upload } = req.file);
+// console.log('this is file', req.file);
+// console.log('this is req.body', req.body);
+// let result;
+// const newFilename = `${req.file.filename}-${req.file.originalname}`;
+//   fs.rename(req.file.path, `${req.file.destination}${newFilename}`, async (err) => {
+//     if (err) return res.send(`ERROR: ${err}`);
 
-    result = await pool.query('insert into images(title,images) values($1,$2) returning *;', [req.file, req.body]);
+//     result = await pool.query('insert into images(title,images) values($1,$2) returning *;', [req.file, req.body]);
 
-    // // req.file.title,
-    // // newFilename;
-  });
-  console.log('this is result', result);
-  return res.status(200).send({ success: result.rows });
-});
+//     // // req.file.title,
+//     // // newFilename;
+//   });
+//   console.log('this is result', result);
+//   return res.status(200).send({ success: result.rows });
+// });
 
-router.use('*', (error, req, res) => {
-  console.log(error);
-  res.send('server not available');
-});
+// router.use('*', (error, req, res) => {
+//   console.log(error);
+//   res.send('server not available');
+// });
 export default router;
