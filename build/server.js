0437236439acd1047ebc1a3e03070d5ff8295d8d
phantware <phantmoney2011@gmail.com>
2020-03-19 08:27:39 +0100

    upload location changed from hardcoded absolute to dynamics

--- build/server.js
+++ build/server.js
@@ -4,6 +4,7 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 
 var _app = _interopRequireDefault(require("./app"));
 
+console.log(__dirname);
 var port = process.env.PORT;
 
 _app["default"].listen(port, console.log("App is listening at ".concat(port)));
--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import app from './app';
 
+console.log(__dirname);
+
 const port = process.env.PORT;
 app.listen(port, console.log(`App is listening at ${port}`));
